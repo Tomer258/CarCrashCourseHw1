5c2442fabad9204f5393db59870a42108419ea1c
Tomer258 <tomer25898@gmail.com>
2023-03-19 14:00:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/carcrashcoursehw1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.carcrashcoursehw1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button startGameBTN;
+
+        startGameBTN=findViewById(R.id.main_BTN_startGame);
+        startGameBTN.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this,game_content.class));
+            finish();
+        });
+    }
+
+}
