482e1ae01700f92c239a951b83f2c9a255ec4ae6
Tomer258 <tomer25898@gmail.com>
2023-03-21 16:46:06 +0200

    added sine game logic (Lane)

---
+++ app/src/main/java/com/example/carcrashcoursehw1/Logic/Lane.java
@@ -0,0 +1,50 @@
+package com.example.carcrashcoursehw1.Logic;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.carcrashcoursehw1.R;
+
+public class Lane {
+    private int isCarInLane=0;
+    private ImageView[] objects =new ImageView[8];
+
+    public Lane(int isCarInLane,ImageView[] objects)
+    {
+        if (objects.length!=8)
+            Log.e("Lane Copy","arrays not same size");
+        else {
+            System.arraycopy(objects, 0, this.objects, 0, objects.length);
+        }
+        for (int i = 1; i < this.objects.length-1; i++) {
+            //this.objects[i].setVisibility(View.INVISIBLE);
+        }
+        if (isCarInLane==1)
+            this.objects[objects.length-1].setImageResource(R.drawable.car);
+        else {
+            this.objects[objects.length - 1].setImageResource(R.drawable.plyo_w39i_210318_ss4mp_generated);
+            //this.objects[objects.length - 1].setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void setCarInLane(int carToggle)
+    {
+        if (carToggle==1)
+            this.objects[7].setImageResource(R.drawable.car);
+        else {
+            this.objects[7].setImageResource(R.drawable.plyo_w39i_210318_ss4mp_generated);
+            //this.objects[objects.length - 1].setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void setDeerVisibility(int index,String mode)
+    {
+        if (index>=0&&index<=7)
+            if (mode.equals("off"))
+                this.objects[index].setVisibility(View.INVISIBLE);
+            else if (mode.equals("on"))
+                this.objects[index].setVisibility(View.VISIBLE);
+    }
+
+}
--- app/src/main/java/com/example/carcrashcoursehw1/game_content.java
+++ app/src/main/java/com/example/carcrashcoursehw1/game_content.java
@@ -3,12 +3,38 @@ package com.example.carcrashcoursehw1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.example.carcrashcoursehw1.Logic.Lane;
 
 public class game_content extends AppCompatActivity {
-
+    private Lane mLane1,mLane2,mLane3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_content);
+        initialLanes();
+
+    }
+
+    private void initialLanes() {
+        ImageView[] iLane1 ={findViewById(R.id.firstLaneDeer1),findViewById(R.id.firstLaneDeer2),
+                            findViewById(R.id.firstLaneDeer3),findViewById(R.id.firstLaneDeer4),
+                            findViewById(R.id.firstLaneDeer5),findViewById(R.id.firstLaneDeer6),
+                            findViewById(R.id.firstLaneDeer7),findViewById(R.id.specialPosFirstLane)};
+
+        ImageView[] iLane2 ={findViewById(R.id.SecondLaneDeer1),findViewById(R.id.SecondLaneDeer2),
+                findViewById(R.id.SecondLaneDeer3),findViewById(R.id.SecondLaneDeer4),
+                findViewById(R.id.SecondLaneDeer5),findViewById(R.id.SecondLaneDeer6),
+                findViewById(R.id.SecondLaneDeer7),findViewById(R.id.specialPosSecondLane)};
+
+        ImageView[] iLane3 ={findViewById(R.id.ThirdLaneDeer1),findViewById(R.id.ThirdLaneDeer2),
+                findViewById(R.id.ThirdLaneDeer3),findViewById(R.id.ThirdLaneDeer4),
+                findViewById(R.id.ThirdLaneDeer5),findViewById(R.id.ThirdLaneDeer6),
+                findViewById(R.id.ThirdLaneDeer7),findViewById(R.id.specialPosThirdLane)};
+
+        mLane1= new Lane(0,iLane1);
+        mLane2= new Lane(1,iLane2);
+        mLane3= new Lane(0,iLane3);
     }
 }
